7d1ca1f4c43f7e7bb79d673855969cc6ece6db0d
Aerys12 <aerys.nsubuga@mail.utoronto.ca>
2023-05-31 11:42:25 -0400

    testimonials removed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 //"use client";
 import Link from "next/link";
 import Image from "next/image";
-import Testimonials from "./components/Testimonials";
 
 export default async function Home() {
 	return (
