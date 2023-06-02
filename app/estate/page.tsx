657fac597e985e1c99a37a99c9020080506534a7
Aerys12 <aerys.nsubuga@mail.utoronto.ca>
2023-06-02 13:17:28 -0400

    working on estate page

--- app/estate/page.tsx
+++ app/estate/page.tsx
@@ -3,30 +3,7 @@ import Link from "next/link";
 export default async function Estate() {
 	return (
 		<div className='mx-auto flex w-full flex-col items-center justify-center lg:max-w-7xl'>
-			<div className='flex flex-col items-center justify-center px-2 pb-4 md:px-8'>
-				<h1 className='p-4 text-4xl'>Real Estate and Interior Photography</h1>
-				<p className='text-center'>
-					Welcome to my real estate and interior photography page! I have a
-					passion for showcasing properties in their best light. Whether you're
-					a real estate agent or homeowner, I can provide stunning images that
-					will make your property stand out. Let's work together to create
-					images that will make your space shine.
-				</p>
-				<div className='flex w-full items-center justify-center px-2 py-2 md:gap-4 md:px-8 md:py-4'>
-					<Link
-						className='flex flex-1 items-center justify-center text-center underline underline-offset-8'
-						href='/'
-					>
-						BOOK YOUR SESSION
-					</Link>
-					<Link
-						className='flex flex-1 items-center justify-center text-center underline underline-offset-8'
-						href='/'
-					>
-						EMAIL US A QUESTION
-					</Link>
-				</div>
-			</div>
+			<h1>This is the estate home</h1>
 		</div>
 	);
 }
--- app/portraits/page.tsx
+++ app/portraits/page.tsx
@@ -3,8 +3,134 @@ import Link from "next/link";
 
 export default async function Portraits() {
 	return (
-		<div className='flex justify-center items-center'>
-			<h1>this is the portraits home</h1>
-		</div>
+		<main className='mx-auto flex w-full flex-col items-center justify-center lg:max-w-7xl'>
+			<div className='flex flex-col items-center justify-center px-2 pb-4 md:px-8'>
+				<h1 className='p-4 text-4xl'>Portrait Photography</h1>
+				<p className='text-center'>
+					Welcome to my portrait photography page! I specialize in capturing the
+					unique essence of each person I photograph, creating images that truly
+					reflect their personality and style. Let's work together to tell your
+					story through my lens.
+				</p>
+				<div className='flex w-full items-center justify-center px-2 py-2 md:gap-4 md:px-8 md:py-4'>
+					<Link
+						className='flex flex-1 items-center justify-center text-center underline underline-offset-8'
+						href='/contact'
+					>
+						BOOK YOUR PORTRAIT SESSION
+					</Link>
+					<Link
+						className='flex flex-1 items-center justify-center text-center underline underline-offset-8'
+						href='/contact'
+					>
+						EMAIL US A QUESTION
+					</Link>
+				</div>
+			</div>
+			<section className='mb-10 grid h-1/4 w-full grid-cols-1 gap-12 p-2 md:grid-cols-3 md:px-8'>
+				<Link
+					className='flex w-full flex-col items-center justify-center'
+					href='/portraits/Couples'
+				>
+					<div className='flex h-64 w-full'>
+						<Image
+							src={
+								"https://d2kukao61u3x53.cloudfront.net/portraits-hero/couple.jpg"
+							}
+							className='h-64 w-full object-cover'
+							alt='this is a portrait'
+							width={500}
+							height={500}
+						/>
+					</div>
+					<h1 className='py-6 text-xl'>Couples</h1>
+				</Link>
+				<Link
+					className='flex w-full flex-col items-center justify-center'
+					href='/portraits/Branding'
+				>
+					<div className='flex h-64 w-full flex-1 object-cover '>
+						<Image
+							className='h-64 w-full object-cover'
+							src={
+								"https://d2kukao61u3x53.cloudfront.net/portraits-hero/branding.jpg"
+							}
+							alt='this is a portrait'
+							width={500}
+							height={500}
+						/>
+					</div>
+					<h1 className='py-6 text-xl'>Branding</h1>
+				</Link>
+				<Link
+					className='flex w-full flex-col items-center justify-center'
+					href='/portraits/Kids'
+				>
+					<div className='flex w-full flex-1 object-cover '>
+						<Image
+							className='h-64 w-full object-cover'
+							src={
+								"https://d2kukao61u3x53.cloudfront.net/portraits-hero/kid.jpg"
+							}
+							alt='this is a portrait'
+							width={500}
+							height={500}
+						/>
+					</div>
+					<h1 className='py-6 text-xl'>Kids</h1>
+				</Link>
+				<Link
+					className='flex w-full flex-col items-center justify-center'
+					href='/portraits/Family'
+				>
+					<div className='flex w-full flex-1 object-cover'>
+						<Image
+							className='h-64 w-full object-cover'
+							src={
+								"https://d2kukao61u3x53.cloudfront.net/portraits-hero/family.jpg"
+							}
+							alt='this is a portrait'
+							width={500}
+							height={500}
+						/>
+					</div>
+					<h1 className='py-6 text-xl'>Family</h1>
+				</Link>
+				<Link
+					className='flex w-full flex-col items-center justify-center'
+					href='/portraits/Maternity'
+				>
+					<div className='flex w-full flex-1 object-cover'>
+						<Image
+							className='h-64 w-full object-cover'
+							src={
+								"https://d2kukao61u3x53.cloudfront.net/portraits-hero/maternity.jpg"
+							}
+							alt='this is a portrait'
+							width={500}
+							height={500}
+						/>
+					</div>
+					<h1 className='py-6 text-xl'>Maternity</h1>
+				</Link>
+				<Link
+					className='flex w-full flex-col items-center justify-center'
+					href='/portraits/Newborn'
+				>
+					<div className='flex w-full flex-1 object-cover'>
+						<Image
+							className='h-64 w-full object-cover'
+							src={
+								"https://d2kukao61u3x53.cloudfront.net/portraits-hero/newborn.jpg"
+							}
+							alt='this is a portrait'
+							width={500}
+							height={500}
+						/>
+					</div>
+					<h1 className='py-6 text-xl'>Newborn</h1>
+				</Link>
+			</section>
+		</main>
 	);
 }
