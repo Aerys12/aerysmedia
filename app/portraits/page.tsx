1ba076551c7b43b3bd14c573457895545a20e859
Aerys12 <aerys.nsubuga@mail.utoronto.ca>
2023-06-02 12:00:22 -0400

    added portraits page

--- app/portraits/page.tsx
+++ app/portraits/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default async function Portraits() {
 	return (
 		<div className='flex justify-center items-center'>
-			This is the home page
+			<h1>this is the portraits home</h1>
 		</div>
 	);
 }
