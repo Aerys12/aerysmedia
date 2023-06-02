f9700c9dbb7d58ce54bef3183689a5df78033fcb
Aerys12 <aerys.nsubuga@mail.utoronto.ca>
2023-06-02 13:27:12 -0400

    working on estate page

--- app/estate/page.tsx
+++ app/estate/page.tsx
@@ -1,17 +1,50 @@
 import Link from "next/link";
+import Gallery from "../components/Gallery";
+import { prisma } from "../db";
 
 export default async function Estate() {
+	const images = await getData();
+
 	return (
-		<div className='mx-auto flex w-full flex-col items-center justify-center lg:max-w-7xl'>
+		<main className='mx-auto flex w-full flex-col items-center justify-center lg:max-w-7xl'>
 			<div className='flex flex-col items-center justify-center px-2 pb-4 md:px-8'>
-				<h1 className='p-4 text-4xl'>Portrait Photography</h1>
+				<h1 className='p-4 text-4xl'>Real Estate and Interior Photography</h1>
 				<p className='text-center'>
-					Welcome to my portrait photography page! I specialize in capturing the
-					unique essence of each person I photograph, creating images that truly
-					reflect their personality and style. work together to tell your story
-					through my lens.
+					Welcome to my real estate and interior photography page! I have a
+					passion for showcasing properties in their best light. Whether you're
+					a real estate agent or homeowner, I can provide stunning images that
+					will make your property stand out. Let us work together to create
+					images that will make your space shine.
 				</p>
+				<div className='flex w-full items-center justify-center px-2 py-2 md:gap-4 md:px-8 md:py-4'>
+					<Link
+						className='flex flex-1 items-center justify-center text-center underline underline-offset-8'
+						href='/contact'
+					>
+						BOOK YOUR SESSION
+					</Link>
+					<Link
+						className='flex flex-1 items-center justify-center text-center underline underline-offset-8'
+						href='/contact'
+					>
+						EMAIL US A QUESTION
+					</Link>
+				</div>
 			</div>
-		</div>
+
+			<section>
+				<Gallery images={images} />
+			</section>
+		</main>
 	);
+}
+
+async function getData() {
+	const images = await prisma?.image.findMany({
+		where: {
+			galleryName: "couples",
+		},
+	});
+
+	return images;
 }
