068d3cf2551bcc08d59646bd8149f088ab551b56
Aerys12 <aerys.nsubuga@mail.utoronto.ca>
2023-06-05 13:48:07 -0400

    blur images loading

--- app/components/Gallery.tsx
+++ app/components/Gallery.tsx
@@ -1,7 +1,11 @@
+"use client";
 import Image from "next/image";
-import { GalleryProps, GalleryImage } from "../types";
+import { GalleryProps } from "../types";
+import { useState } from "react";
+import cn from "classnames";
 
 export default function Gallery({ images }: GalleryProps) {
+	const [isLoading, setLoading] = useState(true);
 	return (
 		<div className='w-full columns-1 gap-1 space-y-1 px-2 md:columns-2 md:px-8 lg:columns-3'>
 			{images?.map((image) => (
@@ -10,11 +14,17 @@ export default function Gallery({ images }: GalleryProps) {
 					className='relative break-inside-avoid overflow-hidden'
 				>
 					<Image
-						className='z-1 object-contain  transition-all hover:scale-125 duration-500'
 						src={`https://${image.imageUrl}`}
 						alt={image.alt}
 						width={500}
 						height={500}
+						className={cn(
+							"z-1 object-contain  transition-all hover:scale-125 duration-500",
+							isLoading
+								? "grayscale blur-2xl scale-110"
+								: "grayscale-0 blur-0 scale-100"
+						)}
+						onLoadingComplete={() => setLoading(false)}
 					/>
 				</div>
 			))}
